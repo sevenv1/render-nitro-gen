d7a657ca9af8ad031875d4402454beb04f65ed2c
seven <134447205+sevenv1@users.noreply.github.com>
2024-03-05 19:36:28 -0500

    Update nitro.py

--- nitro.py
+++ nitro.py
@@ -53,34 +53,34 @@ watermark = """
 """
 
 print(purple + watermark)
-print(f"{purple}[+] made by sevenv1")
+print(f"{purple}[$] made by sevenv1 | my discord server (gonna release a discord tool soon) : https://discord.gg/3j83VKCknY")
 
 choice = input(f"{purple} Press 'Enter' to start the nitro generator: ")
 
-if choice == '':
-    setTitle("Nitro Gen ")
-    
-    print("join the discord im gonna release a discord tool soon | https://discord.gg/3j83VKCknY")
-    
+setTitle("Nitro Generator")
+
+if choice = ''
     webhooklink = str(input(f"{purple} Webhook URL: "))
+    
     validateWebhook(webhooklink)
     
-    count = 0 
+    count = 0
 
-    webhook = "WEBHOOK_URL".replace("WEBHOOK_URL", webhooklink)
+    webhook = "WEBHOOK_URL""".replace("WEBHOOK_URL", webhooklink)
 
-    while count < max_count:
+    while True:
         try:
             code = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(24))
             post = {"content":"https://discord.com/billing/promotions/"+code}
             head = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36", 
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-                'content-type' : 'application/json'
-            }
+
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36", 
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
+                    'content-type' : 'application/json'
+                }
             count += 1
-            print(f"{purple}Generated Nitro | [{count}] .gg/render")
+            print(f'{purple} Generated Nitro | [{count}] .gg/render')
             s = requests.post(webhook, json=post, headers=head)
-        except Exception as e:
-            print(f"{purple}ERROR! {e}")
+        except:
+            print(f'{purple} ERROR!")
             break
